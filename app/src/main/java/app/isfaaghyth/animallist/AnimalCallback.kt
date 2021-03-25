d794ae504e846029c8799708c9ef38b9d66b21f2
isfaaghyth <isfaaghyth@gmail.com>
2021-03-25 15:12:37 +0700

    remove unused class

--- app/src/main/java/app/isfaaghyth/animallist/AnimalCallback.kt
+++
@@ -1,8 +0,0 @@
-package app.isfaaghyth.animallist
-
-import app.isfaaghyth.animallist.datasource.Animal
-
-// interface is the abstract class
-interface AnimalCallback {
-    fun sendAnimal(animal: Animal)
-}
